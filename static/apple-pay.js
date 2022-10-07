35ba4fccac71ad88a3181b84cd5194e5109762ac
Sarah Kaplan <sarahk@wepay.com>
2022-10-07 13:44:02 -0700

    removed sentence about local phoenix

--- static/apple-pay.js
+++ static/apple-pay.js
@@ -18,7 +18,6 @@ const apple_pay_configs = {
         cssVariables: {
             'borderRadius': '5px',
             'padding:': '0px',
-            'color': 'blue',
             'ApplePayButtonType': 'check-out',
             'ApplePayButtonStyle': 'white-outline'
         },
